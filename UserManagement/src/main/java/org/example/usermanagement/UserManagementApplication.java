9be7e9b4c3d51509fb0ebc911e110b859c12e3a4
Mauricio Lugo <maurogranados31@gmail.com>
2025-12-18 10:27:43 -0500

    feat: Hello world

--- UserManagement/src/main/java/org/example/usermanagement/UserManagementApplication.java
+++ UserManagement/src/main/java/org/example/usermanagement/UserManagementApplication.java
@@ -2,11 +2,19 @@ package org.example.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class UserManagementApplication {
 
     void main() { SpringApplication.run(UserManagementApplication.class);
+    }
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello word";
     }
 
 }
