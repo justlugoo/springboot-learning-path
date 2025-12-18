be94bfb5d35a4fac77b44260f091fcc18cf3c4a9
Mauricio Lugo <maurogranados31@gmail.com>
2025-12-18 10:25:23 -0500

    feat: add Role entity with timestamps and description

--- UserManagement/src/main/java/org/example/usermanagement/model/Role.java
+++ UserManagement/src/main/java/org/example/usermanagement/model/Role.java
@@ -1,7 +1,40 @@
 package org.example.usermanagement.model;
 
-public enum Role {
-    USER,
-    MODERATOR,
-    ADMIN
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String name; // User, Moderator, Admin
+
+    @Column(length = 200)
+    private String description;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
 }
